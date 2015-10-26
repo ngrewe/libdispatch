c64234abbdc505cadadb95daf34085c5ffc52048
Nicholas Hutchinson <nshutchinson@gmail.com>
2015-10-26 21:36:11 +0000

    Workqueue: tidy with clang-format

--- src/workqueue_internal.h
+++ src/workqueue_internal.h
@@ -37,22 +37,22 @@
 #define PWQ_EXPORT extern
 #endif
 
-typedef struct _pthread_workqueue * pthread_workqueue_t;
-typedef void *                      pthread_workitem_handle_t;
+typedef struct _pthread_workqueue *pthread_workqueue_t;
+typedef void *pthread_workitem_handle_t;
 
 /* Pad size to 64 bytes. */
 typedef struct {
-    unsigned int sig;
-    int queueprio;
-    int overcommit;
-    unsigned int pad[13];
+	unsigned int sig;
+	int queueprio;
+	int overcommit;
+	unsigned int pad[13];
 } pthread_workqueue_attr_t;
 
 /* Work queue priority attributes. */
-#define WORKQ_HIGH_PRIOQUEUE       0
-#define WORKQ_DEFAULT_PRIOQUEUE    1
-#define WORKQ_LOW_PRIOQUEUE        2
-#define WORKQ_BG_PRIOQUEUE         3
+#define WORKQ_HIGH_PRIOQUEUE 0
+#define WORKQ_DEFAULT_PRIOQUEUE 1
+#define WORKQ_LOW_PRIOQUEUE 2
+#define WORKQ_BG_PRIOQUEUE 3
 
 #if defined(__cplusplus)
 extern "C" {
@@ -60,14 +60,14 @@ extern "C" {
 
 PWQ_EXPORT
 int pthread_workqueue_create_np(pthread_workqueue_t *workqp,
-                                const pthread_workqueue_attr_t *attr);
+								const pthread_workqueue_attr_t *attr);
 
 PWQ_EXPORT
 int pthread_workqueue_additem_np(pthread_workqueue_t workq,
-                                 void (*workitem_func)(void *),
-                                 void *workitem_arg,
-                                 pthread_workitem_handle_t *itemhandlep,
-                                 unsigned int *gencountp);
+								 void (*workitem_func)(void *),
+								 void *workitem_arg,
+								 pthread_workitem_handle_t *itemhandlep,
+								 unsigned int *gencountp);
 
 PWQ_EXPORT
 int pthread_workqueue_attr_init_np(pthread_workqueue_attr_t *attrp);
@@ -77,27 +77,28 @@ int pthread_workqueue_attr_destroy_np(pthread_workqueue_attr_t *attr);
 
 PWQ_EXPORT
 int pthread_workqueue_attr_getqueuepriority_np(pthread_workqueue_attr_t *attr,
-                                               int *qpriop);
+											   int *qpriop);
 
 PWQ_EXPORT
 int pthread_workqueue_attr_setqueuepriority_np(pthread_workqueue_attr_t *attr,
-                                               int qprio);
+											   int qprio);
 
 PWQ_EXPORT
 int pthread_workqueue_attr_getovercommit_np(
-        const pthread_workqueue_attr_t *attr, int *ocommp);
+	const pthread_workqueue_attr_t *attr, int *ocommp);
 
 PWQ_EXPORT
 int pthread_workqueue_attr_setovercommit_np(pthread_workqueue_attr_t *attr,
-                                            int ocomm);
+											int ocomm);
 
 PWQ_EXPORT
 int pthread_workqueue_requestconcurrency_np(pthread_workqueue_t workq,
-                                            int queue, int request_concurrency);
+											int queue,
+											int request_concurrency);
 
 PWQ_EXPORT
 int pthread_workqueue_getovercommit_np(pthread_workqueue_t workq,
-                                       unsigned int *ocommp);
+									   unsigned int *ocommp);
 
 PWQ_EXPORT
 void pthread_workqueue_main_np(void);
@@ -118,8 +119,8 @@ void pthread_workqueue_resume_np(void);
 #endif
 #endif /* _PTHREAD_WORKQUEUE_H */
 
-#ifndef  _DEBUG_H
-#define  _DEBUG_H
+#ifndef _DEBUG_H
+#define _DEBUG_H
 
 #include <assert.h>
 
@@ -130,37 +131,54 @@ extern char *WORKQUEUE_DEBUG_IDENT;
 #include <sys/syscall.h>
 #include <unistd.h>
 
-# define THREAD_ID ((pid_t)  syscall(__NR_gettid))
+#define THREAD_ID ((pid_t)syscall(__NR_gettid))
 
+#define dbg_puts(str)                      \
+	do {                                   \
+		if (DEBUG_WORKQUEUE)               \
+			fprintf(stderr,                \
+					"%s [%d]: %s(): %s\n", \
+					WORKQUEUE_DEBUG_IDENT, \
+					THREAD_ID,             \
+					__func__,              \
+					str);                  \
+	} while (0)
 
-#define dbg_puts(str)           do {                                \
-    if (DEBUG_WORKQUEUE)                                                      \
-      fprintf(stderr, "%s [%d]: %s(): %s\n",                        \
-              WORKQUEUE_DEBUG_IDENT, THREAD_ID, __func__, str);               \
-} while (0)
+#define dbg_printf(fmt, ...)                    \
+	do {                                        \
+		if (DEBUG_WORKQUEUE)                    \
+			fprintf(stderr,                     \
+					"%s [%d]: %s(): " fmt "\n", \
+					WORKQUEUE_DEBUG_IDENT,      \
+					THREAD_ID,                  \
+					__func__,                   \
+					__VA_ARGS__);               \
+	} while (0)
 
-#define dbg_printf(fmt,...)     do {                                \
-    if (DEBUG_WORKQUEUE)                                                      \
-      fprintf(stderr, "%s [%d]: %s(): "fmt"\n",                     \
-              WORKQUEUE_DEBUG_IDENT, THREAD_ID, __func__, __VA_ARGS__);       \
-} while (0)
+#define dbg_perror(str)                                   \
+	do {                                                  \
+		if (DEBUG_WORKQUEUE)                              \
+			fprintf(stderr,                               \
+					"%s [%d]: %s(): %s: %s (errno=%d)\n", \
+					WORKQUEUE_DEBUG_IDENT,                \
+					THREAD_ID,                            \
+					__func__,                             \
+					str,                                  \
+					strerror(errno),                      \
+					errno);                               \
+	} while (0)
 
-#define dbg_perror(str)         do {                                \
-    if (DEBUG_WORKQUEUE)                                                      \
-      fprintf(stderr, "%s [%d]: %s(): %s: %s (errno=%d)\n",         \
-              WORKQUEUE_DEBUG_IDENT, THREAD_ID, __func__, str,                \
-              strerror(errno), errno);                              \
-} while (0)
+#define reset_errno() \
+	do {              \
+		errno = 0;    \
+	} while (0)
 
-# define reset_errno()          do { errno = 0; } while (0)
+#define dbg_lasterror(str) ;
 
-#  define dbg_lasterror(str)     ;
-
-#endif  /* ! _DEBUG_H */
+#endif /* ! _DEBUG_H */
 
 #ifndef _PTWQ_PRIVATE_H
 #define _PTWQ_PRIVATE_H 1
-
 
 /* The maximum number of workqueues that can be created.
    This is based on libdispatch only needing 8 workqueues.
@@ -171,42 +189,45 @@ extern char *WORKQUEUE_DEBUG_IDENT;
 #define WORKQ_NUM_PRIOQUEUE 4
 
 /* Signatures/magic numbers.  */
-#define PTHREAD_WORKQUEUE_SIG       0xBEBEBEBE
-#define PTHREAD_WORKQUEUE_ATTR_SIG  0xBEBEBEBE 
+#define PTHREAD_WORKQUEUE_SIG 0xBEBEBEBE
+#define PTHREAD_WORKQUEUE_ATTR_SIG 0xBEBEBEBE
 
 /* Whether to use real-time threads for the workers if available */
 
 extern unsigned int PWQ_RT_THREADS;
 extern unsigned int PWQ_SPIN_THREADS;
 
-/* A limit of the number of cpu:s that we view as available, useful when e.g. using processor sets */
+/* A limit of the number of cpu:s that we view as available, useful when e.g.
+ * using processor sets */
 extern unsigned int PWQ_ACTIVE_CPU;
 
 #if __GNUC__
-#define fastpath(x)     ((__typeof__(x))__builtin_expect((long)(x), ~0l))
-#define slowpath(x)     ((__typeof__(x))__builtin_expect((long)(x), 0l))
+#define fastpath(x) ((__typeof__(x))__builtin_expect((long)(x), ~0l))
+#define slowpath(x) ((__typeof__(x))__builtin_expect((long)(x), 0l))
 #else
 #define fastpath(x) (x)
 #define slowpath(x) (x)
 #endif
 
-#define CACHELINE_SIZE	64
-#define ROUND_UP_TO_CACHELINE_SIZE(x)	(((x) + (CACHELINE_SIZE - 1)) & ~(CACHELINE_SIZE - 1))
+#define CACHELINE_SIZE 64
+#define ROUND_UP_TO_CACHELINE_SIZE(x) \
+	(((x) + (CACHELINE_SIZE - 1)) & ~(CACHELINE_SIZE - 1))
 
-/* We should perform a hardware pause when using the optional busy waiting, see: 
-   http://software.intel.com/en-us/articles/ap949-using-spin-loops-on-intel-pentiumr-4-processor-and-intel-xeonr-processor/ 
+/* We should perform a hardware pause when using the optional busy waiting, see:
+   http://software.intel.com/en-us/articles/ap949-using-spin-loops-on-intel-pentiumr-4-processor-and-intel-xeonr-processor/
  rep/nop / 0xf3+0x90 are the same as the symbolic 'pause' instruction
  */
 
-#if defined(__i386__) || defined(__x86_64__) || defined(__i386) || defined(__amd64)
+#if defined(__i386__) || defined(__x86_64__) || defined(__i386) || \
+	defined(__amd64)
 
 #if defined(__SUNPRO_CC)
 
-#define _hardware_pause()  asm volatile("rep; nop\n");
+#define _hardware_pause() asm volatile("rep; nop\n");
 
 #elif defined(__GNUC__)
 
-#define _hardware_pause()  __asm__ __volatile__("pause");
+#define _hardware_pause() __asm__ __volatile__("pause");
 
 #else
 
@@ -224,34 +245,40 @@ extern unsigned int PWQ_ACTIVE_CPU;
 /* XXX-FIXME this is a stub, need to research what assembly to use */
 #define _hardware_pause() __asm__("")
 
-#endif 
+#endif
 
 /*
  * The work item cache, has three different optional implementations:
  * 1. No cache, just normal malloc/free using the standard malloc library in use
- * 2. Libumem based object cache, requires linkage with libumem - for non-Solaris see http://labs.omniti.com/labs/portableumem
- *    this is the most balanced cache supporting migration across threads of allocated/freed witems
- * 3. TSD based cache, modelled on libdispatch continuation implementation, can lead to imbalance with assymetric 
- *    producer/consumer threads as allocated memory is cached by the thread freeing it
+ * 2. Libumem based object cache, requires linkage with libumem - for
+ * non-Solaris see http://labs.omniti.com/labs/portableumem
+ *    this is the most balanced cache supporting migration across threads of
+ * allocated/freed witems
+ * 3. TSD based cache, modelled on libdispatch continuation implementation, can
+ * lead to imbalance with assymetric
+ *    producer/consumer threads as allocated memory is cached by the thread
+ * freeing it
  */
-#define WITEM_CACHE_TYPE 1 // Otherwise fallback to normal malloc/free - change specify witem cache implementation to use
+#define WITEM_CACHE_TYPE \
+	1  // Otherwise fallback to normal malloc/free - change specify witem cache
+	   // implementation to use
 
 struct work {
-    STAILQ_ENTRY(work)   item_entry; 
-    void               (*func)(void *);
-    void                *func_arg;
-    unsigned int         flags;
-    unsigned int         gencount;
+	STAILQ_ENTRY(work) item_entry;
+	void (*func)(void *);
+	void *func_arg;
+	unsigned int flags;
+	unsigned int gencount;
 };
 
 struct _pthread_workqueue {
-    unsigned int         sig;    /* Unique signature for this structure */
-    unsigned int         flags;
-    int                  queueprio;
-    int                  overcommit;
-    unsigned int         wqlist_index;
-    STAILQ_HEAD(,work)   item_listhead;
-    pthread_spinlock_t   mtx;
+	unsigned int sig; /* Unique signature for this structure */
+	unsigned int flags;
+	int queueprio;
+	int overcommit;
+	unsigned int wqlist_index;
+	STAILQ_HEAD(, work) item_listhead;
+	pthread_spinlock_t mtx;
 #ifdef WORKQUEUE_PLATFORM_SPECIFIC
 	WORKQUEUE_PLATFORM_SPECIFIC;
 #endif
@@ -265,26 +292,29 @@ void manager_resume(void);
 void manager_workqueue_create(struct _pthread_workqueue *);
 void manager_workqueue_additem(struct _pthread_workqueue *, struct work *);
 
-struct work *witem_alloc(void (*func)(void *), void *func_arg); // returns a properly initialized witem
+struct work *witem_alloc(
+	void (*func)(void *),
+	void *func_arg);  // returns a properly initialized witem
 void witem_free(struct work *wi);
 int witem_cache_init(void);
 void witem_cache_cleanup(void *value);
 
-#endif  /* _PTWQ_PRIVATE_H */
+#endif /* _PTWQ_PRIVATE_H */
 
 #ifndef _PTWQ_POSIX_THREAD_INFO_H
 #define _PTWQ_POSIX_THREAD_INFO_H 1
 
-int threads_runnable(unsigned int *threads_running, unsigned int *threads_total);
+int threads_runnable(unsigned int *threads_running,
+					 unsigned int *threads_total);
 
-#endif  /* _PTWQ_POSIX_THREAD_INFO_H */
+#endif /* _PTWQ_POSIX_THREAD_INFO_H */
 
 #ifndef _PTWQ_THREAD_RT_H
 #define _PTWQ_THREAD_RT_H 1
 
-void ptwq_set_current_thread_priority(int priority); // higher is better
+void ptwq_set_current_thread_priority(int priority);  // higher is better
 
-#endif  /* _PTWQ_THREAD_RT_H */
+#endif /* _PTWQ_THREAD_RT_H */
 #ifndef _PTWQ_POSIX_PLATFORM_H
 #define _PTWQ_POSIX_PLATFORM_H 1
 
@@ -302,22 +332,22 @@ void ptwq_set_current_thread_priority(int priority); // higher is better
 #include <unistd.h>
 #include <pthread.h>
 
-/* GCC atomic builtins. 
- * See: http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html 
+/* GCC atomic builtins.
+ * See: http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
  */
-# define atomic_inc(p)   (void) __sync_add_and_fetch((p), 1)
-# define atomic_dec(p)   (void) __sync_sub_and_fetch((p), 1)
-# define atomic_inc_nv(p)  __sync_add_and_fetch((p), 1)
-# define atomic_dec_nv(p)  __sync_sub_and_fetch((p), 1)
-# define atomic_and(p,v)   __sync_and_and_fetch((p), (v))
-# define atomic_or(p,v)   __sync_or_and_fetch((p), (v))
+#define atomic_inc(p) (void) __sync_add_and_fetch((p), 1)
+#define atomic_dec(p) (void) __sync_sub_and_fetch((p), 1)
+#define atomic_inc_nv(p) __sync_add_and_fetch((p), 1)
+#define atomic_dec_nv(p) __sync_sub_and_fetch((p), 1)
+#define atomic_and(p, v) __sync_and_and_fetch((p), (v))
+#define atomic_or(p, v) __sync_or_and_fetch((p), (v))
 
 /*
  * Android does not provide spinlocks.
  * See: http://code.google.com/p/android/issues/detail?id=21622
  */
 
-#endif  /* _PTWQ_POSIX_PLATFORM_H */
+#endif /* _PTWQ_POSIX_PLATFORM_H */
 
 #ifndef _LIBPWQ_LINUX_PLATFORM_H
 #define _LIBPWQ_LINUX_PLATFORM_H
@@ -328,7 +358,7 @@ void ptwq_set_current_thread_priority(int priority); // higher is better
 
 unsigned int linux_get_runqueue_length(void);
 
-/* 
+/*
  * Android does not provide spinlocks.
  * See: http://code.google.com/p/android/issues/detail?id=21622
  */
